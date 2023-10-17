38124cfa77faf878f385e2454d6f009d92debe32
Sander Zeeman <szeeman2000@gmail.com>
2023-10-17 16:44:54 +0200

    Modify config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/IEGS2013XRandomizer/"
+  base: process.env.NODE_ENV === "production" ? "/IEGS2013XRandomizer/" : '/'
 })
